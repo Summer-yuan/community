9bd83703a0cfcf270649d24f0c79d759632bc156
summer-yuan <syl17762173178@163.com>
2022-01-16 22:29:38 +0800

    显示系统通知

--- src/main/java/com/nowcoder/community/dao/MessageMapper.java
+++ src/main/java/com/nowcoder/community/dao/MessageMapper.java
@@ -37,4 +37,16 @@ public interface MessageMapper {
 
     //修改消息状态： 已读 删除
     int updateStatus(List<Integer> ids, int status);
+
+    // 查询某个主题下最新的通知   某个人 某主题
+    Message selectLatestNotice(int userId, String topic);
+
+    // 查询某个主题所包含的通知数量
+    int selectNoticeCount(int userId, String topic);
+
+    // 查询未读的通知的数量
+    int selectNoticeUnreadCount(int userId, String topic);
+
+    // 查询某个主题所包含的通知列表
+    List<Message> selectNotices(int userId, String topic, int offset, int limit);
 }
